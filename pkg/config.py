50152ddf41d08770f2d97b6aaf3d9a0784a2876b
Oluseye Adeleke <oluseye.d.adeleke@gmail.com>
2026-09-17 14:24:50 -0700

    fix: normalize Railway MySQL database URL driver

--- pkg/config.py
+++ pkg/config.py
@@ -1,5 +1,11 @@
 import os
 
+def _get_database_uri():
+    db_url = os.environ.get('DATABASE_URL', 'mysql+mysqlconnector://root:rootpassword@127.0.0.1/odasitydb')
+    if db_url.startswith('mysql://'):
+        return 'mysql+mysqlconnector://' + db_url[8:]
+    return db_url
+
 class GeneralConfig(object):
     SECRET_KEY = os.environ.get('SECRET_KEY', 'trMNfGHpxif25uzIYwU')
     TECH_SUPPORT = "0809999999"
@@ -7,12 +13,12 @@ class GeneralConfig(object):
 class ProConfig(GeneralConfig):
     SECRET_KEY = os.environ.get('SECRET_KEY', 'live_trMNfGHpxif25uzIYwU')
     ADMIN_EMAIL = "live@admin.com"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql+mysqlconnector://root:rootpassword@127.0.0.1/odasitydb')
+    SQLALCHEMY_DATABASE_URI = _get_database_uri()
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevConfig(GeneralConfig):
     SECRET_KEY = "dev_trMNfGHpxif25uzIYwU"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql+mysqlconnector://root:rootpassword@127.0.0.1/odasitydb')
+    SQLALCHEMY_DATABASE_URI = _get_database_uri()
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class TestConfig(GeneralConfig):
